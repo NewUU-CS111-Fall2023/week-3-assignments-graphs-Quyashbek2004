eabfe0e088d23f12c9313e0e964df8318c0d8fb0
Quyashbek2004 <127374399+Quyashbek2004@users.noreply.github.com>
2023-10-31 20:44:46 +0500

    Update task_4.h

--- src/cpp/task_4.h
+++ src/cpp/task_4.h
@@ -1,41 +1,39 @@
+
 #include <iostream>
-#include <vector>
+#include <set>
+#include <string>
+#include <algorithm>
 
-using namespace std;
+std::string restoreGenome(const std::vector<std::string>& fragments) {
+    std::set<char> uniqueLetters;
 
-int dfs(int node, int parent, int target, vector<vector<int>>& graph) {
-    if (node == target) {
-        return 0;
-    }
-    
-    for (int neighbor : graph[node]) {
-        if (neighbor != parent) {
-            int distance = dfs(neighbor, node, target, graph);
-            if (distance >= 0) {
-                return distance + 1;
-            }
+
+    for (const std::string& fragment : fragments) {
+        for (char letter : fragment) {
+            uniqueLetters.insert(letter);
         }
     }
-    
-    return -1;
+
+    std::string genome;
+    for (char letter : uniqueLetters) {
+        genome += letter;
+    }
+    std::sort(genome.begin(), genome.end());
+
+    return genome;
 }
 
 int main() {
-    int n, x;
-    cin >> n >> x;
-    
-    vector<vector<int>> graph(n+1);
-    
-    for (int i = 0; i < n-1; i++) {
-        int a, b;
-        cin >> a >> b;
-        graph[a].push_back(b);
-        graph[b].push_back(a);
+    int n;
+    std::cin >> n;
+
+    std::vector<std::string> fragments(n);
+    for (int i = 0; i < n; i++) {
+        std::cin >> fragments[i];
     }
-    
-    int distance = dfs(1, 0, x, graph);
-    
-    cout << distance * 2 << endl;
-    
+
+    std::string genome = restoreGenome(fragments);
+    std::cout << genome << std::endl;
+
     return 0;
 }
